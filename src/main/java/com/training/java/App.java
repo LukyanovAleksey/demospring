5a7badb2fa6083986e1031600b52f9ca47032694
a.lukyanov <a.lukyanov92@yandex.ru>
2020-09-18 10:16:51 +0400

    Initial commit

---
+++ src/main/java/com/training/java/App.java
@@ -0,0 +1,12 @@
+package com.training.java;
+
+/**
+ * App.
+ *
+ * @author Aleksey_Lukyanov
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
