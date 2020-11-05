2d514c85eb0cdbc38a89e300598d6657bf148710
a.lukyanov <a.lukyanov92@yandex.ru>
2020-11-05 10:05:45 +0400

    Added view showUser

--- src/main/java/com/training/java/dao/UserDao.java
+++ src/main/java/com/training/java/dao/UserDao.java
@@ -11,6 +11,14 @@ import java.util.List;
  */
 public interface UserDao {
 
+    void save(User user);
+
+    User getById(int id);
+
+    void delete(int id);
+
+    void update(User user);
+
     List<User> findAll();
 
 }
--- src/main/java/com/training/java/service/UserServiceImpl.java
+++ src/main/java/com/training/java/service/UserServiceImpl.java
@@ -21,4 +21,20 @@ public class UserServiceImpl implements UserService {
     public List<User> findAll() {
         return userDao.findAll();
     }
+
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+    public User getById(int id) {
+        return userDao.getById(id);
+    }
+
+    public void delete(int id) {
+        userDao.delete(id);
+    }
+
+    public void update(User user) {
+        userDao.update(user);
+    }
 }
